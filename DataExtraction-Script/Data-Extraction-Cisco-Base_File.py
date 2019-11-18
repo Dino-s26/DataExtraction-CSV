7872c77d08f3d5df8861fc4d7330a3f351d6542e
Alldino Syaman <alldino.syaman@gmail.com>
2019-11-18 16:22:57 +0700

    Update Data-Extraction-Cisco-Base_File.py
    + update regex

--- DataExtraction-Script/Data-Extraction-Cisco-Base_File.py
+++ DataExtraction-Script/Data-Extraction-Cisco-Base_File.py
@@ -31,10 +31,18 @@ for file in source_file:
  ## Begin Data Extraction for specific information using defined regex
  # Change this line below with your defined regex, you may add more regex to this line
  hostname = re.findall('hostname.*', line_re)
- cpu_ios_xe = re.findall('Core \d*\:.CPU.utilization.for.five.seconds\:.\d*\%\;.one.minute\:.\d*\%\;..five.minutes\:.\d*\%', line_re)
- cpu_ios = re.findall('CPU.utilization.for.five.seconds\:.\d*\%.\d*\%\;.one.minute\:.\d*\%\;.five.minutes\:.\d*\%', line_re)
+ hostname2 = re.findall('WSA2[-]\w*[#]|WSA2[-]\w*[-#]\w*[-#12]\w*[-#]\w*', line_re)
+ cpu_ios_xe = re.findall('five minutes\: \d*\%', line_re)
+ cpu_ios = re.findall('[;].five.minutes\:.\d*\%', line_re)
+ cpu_ios_1 = re.findall('[;] five minutes: \d*%', line_re)
  process_memory = re.findall('Processor.Pool.Total:..\d*.Used:..\d*.Free:..\d*', line_re)
  process_memory_xe = re.findall('System.memory..:.\d*K.total,.\d*K.used,.\d*K.free,.\d*K.kernel.reserved', line_re)
+ process_memory_94K = re.findall('System.memory:.\d*K.total,.\d*K.used,.\d*K.free',line_re)
+ cpu_asa = re.findall('[;].5.minutes:.\d*%', line_re)
+ memory_asa = re.findall('.bytes.[(](.|..\d*%.)[)]', line_re)
+ memory_asa2 = re.findall('Used memory:........\d*.bytes.[(]\d*%[)]', line_re)
+ cpu_nx = re.findall('CPU states  :...\d*[.]\d*% user[,]...\d*[.]\d*% kernel[,]...\d*[.]\d*% idle', line_re)
+ memory_nx = re.findall('Memory usage:...\d*K total[,]...\d*K used[,]...\d*K free', line_re)
  
  ## For Debug Purpose please uncomment below line \/,
  # and comment the Extract Data line code to prevent from save unnecessary data, add more 
@@ -46,9 +54,17 @@ for file in source_file:
  ## Extract Data and write it to CSV File
  # Comment this line below when enable Debug Line
  with open(name, 'a') as csvfile:
-      wrm = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
-      wrm.writerow(hostname)
-      wrm.writerow(cpu_ios_xe)
-      wrm.writerow(cpu_ios)
-      wrm.writerow(process_memory)
-      wrm.writerow(process_memory_xe)
+      filewriter = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
+      filewriter.writerow(hostname)
+      filewriter.writerow(hostname2)
+      filewriter.writerow(cpu_ios_xe)
+      filewriter.writerow(cpu_ios)
+      filewriter.writerow(cpu_ios_1)
+      filewriter.writerow(process_memory)
+      filewriter.writerow(process_memory_xe)
+      filewriter.writerow(process_memory_94K)
+      filewriter.writerow(cpu_asa)
+      filewriter.writerow(memory_asa)
+      filewriter.writerow(memory_asa2)
+      filewriter.writerow(cpu_nx)
+      filewriter.writerow(memory_nx)
